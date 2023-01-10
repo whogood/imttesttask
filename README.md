ab29d8745e01b9ebeffaeaf4e4446540ea087e58
Sergey Ermolaev <whogood@gmail.com>
2023-01-10 14:51:57 +0200

    Make tesk task

---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+
+Tesk task
+
+# Installation
+- `composer install`
+- Copy `config.sample.yml` to `config.yml` and define config file
---
+++ backend/Utils.php
@@ -0,0 +1,19 @@
+<?php
+namespace App;
+
+class Utils
+{
+    /*
+     * Get real client IP address
+     * */
+    static public function getIpAddress()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            return $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
